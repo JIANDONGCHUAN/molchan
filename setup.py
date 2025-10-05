196632524c5aba600a066e154862d39424f90621
JIANDONGCHUAN <55935506+JIANDONGCHUAN@users.noreply.github.com>
2025-10-05 11:45:22 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='molchan',
+    version='0.1',
+    author='simon williams'
+    url='https://github.com/JIANDONGCHUAN/molchan',
+    authors='Simon Williams',
+    #author_email='siwill22@gmail.com',
+    #license='MIT',
+    packages=find_packages(),
+)
