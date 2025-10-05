77df08307f95d3d138c4cd4835b38a09fc3d3d30
JIANDONGCHUAN <55935506+JIANDONGCHUAN@users.noreply.github.com>
2025-10-05 11:48:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='molchan',
     version='0.1',
-    author='simon williams'
     url='https://github.com/JIANDONGCHUAN/molchan',
     authors='Simon Williams',
     #author_email='siwill22@gmail.com',
